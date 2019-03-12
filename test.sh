65b49eae502e37ab56626445df49eba6729f4713
Fernando Aguilar <ferag@live.com>
2019-03-12 17:54:43 +0100

    Update ACCESS_TOKEN before getting onedata

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+export OAUTH2_AUTHORIZE_TOKEN=$(curl -X POST -H "Content-Type: application/json" -s "https://iam.extreme-datacloud.eu/token?grant_type=refresh_token&refresh_token=$OAUTH2_REFRESH_TOKEN&client_id=$IAM_CLIENT_ID&client_secret=$IAM_CLIENT_SECRET" | python -c "import sys, json; print json.load(sys.stdin)['access_token']")
 curl -H "X-Auth-Token: eXtreme-DataCloud:$OAUTH2_AUTHORIZE_TOKEN" -s "$ONEDATA_ZONE/api/v3/onezone/user/client_tokens"
-ONEDATA_TOKEN=$(curl -H "X-Auth-Token: eXtreme-DataCloud:$OAUTH2_AUTHORIZE_TOKEN" -s "$ONEDATA_ZONE/api/v3/onezone/user/client_tokens" | python -c "import sys, json; print json.load(sys.stdin)['tokens'][0]")
+export ONEDATA_TOKEN=$(curl -H "X-Auth-Token: eXtreme-DataCloud:$OAUTH2_AUTHORIZE_TOKEN" -s "$ONEDATA_ZONE/api/v3/onezone/user/client_tokens" | python -c "import sys, json; print json.load(sys.stdin)['tokens'][0]")
 export ONECLIENT_AUTHORIZATION_TOKEN=$ONEDATA_TOKEN
 sudo chown -R jovyan:users /home/jovyan/.local/share/jupyter
 sudo chown -R jovyan:users /home/jovyan/datasets
