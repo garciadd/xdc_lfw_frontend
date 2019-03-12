681cad83e40f531168f6dad8e63e9a0e1905fd2b
Fernando Aguilar <ferag@live.com>
2019-03-12 17:26:13 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+curl -H "X-Auth-Token: eXtreme-DataCloud:$OAUTH2_AUTHORIZE_TOKEN" -s "$ONEDATA_ZONE/api/v3/onezone/user/client_tokens"
 ONEDATA_TOKEN=$(curl -H "X-Auth-Token: eXtreme-DataCloud:$OAUTH2_AUTHORIZE_TOKEN" -s "$ONEDATA_ZONE/api/v3/onezone/user/client_tokens" | python -c "import sys, json; print json.load(sys.stdin)['tokens'][0]")
 export ONECLIENT_AUTHORIZATION_TOKEN=$ONEDATA_TOKEN
 sudo chown -R jovyan:users /home/jovyan/.local/share/jupyter
