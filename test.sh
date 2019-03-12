16c02613668f14e4e7646563082244b402139edb
Fernando Aguilar <ferag@live.com>
2019-03-12 12:54:55 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 ONEDATA_TOKEN=$(curl -H "X-Auth-Token: eXtreme-DataCloud:$OAUTH2_AUTHORIZE_TOKEN" -s "$ONEDATA_ZONE/api/v3/onezone/user/client_tokens" | python -c "import sys, json; print json.load(sys.stdin)['tokens'][0]")
-ONECLIENT_AUTHORIZATION_TOKEN=$ONEDATA_TOKEN
+export ONECLIENT_AUTHORIZATION_TOKEN=$ONEDATA_TOKEN
 sudo chown -R jovyan:users /home/jovyan/.local/share/jupyter
 sudo chown -R jovyan:users /home/jovyan/datasets
 su jovyan -c 'oneclient --authentication token --no_check_certificate /home/jovyan/datasets'
